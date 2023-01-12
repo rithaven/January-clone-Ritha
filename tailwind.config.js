9775575f0d7e0830375e6b1f75872b220f160ea1
rithaven <veneruth4@gmail.com>
2023-01-12 11:41:36 +0200

    Add menu section

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,21 @@ module.exports = {
     "./Components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        xl: { max: "1279px" },
+        // => @media (max-width: 1279px) { ... }
+  
+        lg: { max: "1023px" },
+        // => @media (max-width: 1023px) { ... }
+  
+        md: { max: "767px" },
+        // => @media (max-width: 767px) { ... }
+  
+        sm: { max: "639px" },
+        // => @media (max-width: 639px) { ... }
+      },
+    },
   },
   plugins: [],
 }
